14a60c8bcbb9021faea10e504d30feec5e82966f
DeLongYang <1157013577@qq.com>
2017-09-27 10:34:57 +0800

    Initial Commit

---
+++ ShareElement_DesginMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareElement_DesginMode
+//
+//  Created by DeLongYang on 2017/9/27.
+//  Copyright © 2017年 DeLongYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShareElement_DesginMode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShareElement_DesginMode
+//
+//  Created by DeLongYang on 2017/9/27.
+//  Copyright © 2017年 DeLongYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
